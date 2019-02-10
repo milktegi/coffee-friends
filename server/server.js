0ed9235a0163e49c18059d033db11ea5b602a976
milktegi <milkteegi@gmail.com>
2019-02-10 16:09:35 +0900

    created login router

--- server/server.js
+++ server/server.js
@@ -22,6 +22,15 @@ app.post('/api/users/register', (req,res)=>{
 	})
 })
 
+app.post('/api/users/login', (req, res)=>{
+	 // 이메일 찾고 
+	 User.findOne({'email':req.body.email }, (err, user)=>{
+		 if(!user) return res.json({ loginSucess: false, err })
+	 })
+	 // 패스워드 체크하고 
+	 // 토큰을 생성한다 
+})
+
 const port = process.env.PORT || 3002; 
 app.listen(port, ()=>{
   console.log(`server running at ${port}`)
